4766f2c940bb78d5a28001525454369f298f7217
Rafael Perez <leafarfernandez@gmail.com>
2023-03-08 13:18:42 -0400

    Inicio de practica

---
+++ components/Layout.js
@@ -0,0 +1,21 @@
+import React from "react";
+import Head from 'next/head';
+import Sidebar from '../components/Sidebar';
+
+const Layout = ({children}) => {
+    return (
+      <>
+          <Head>
+              <title>CRM - Administración de Clientes</title>
+              <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css" integrity="sha256-l85OmPOjvil/SOvVt3HnSSjzF1TUMyT9eV0c2BzEGzU=" crossOrigin="anonymous" />
+              <link href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css" rel="stylesheet" />
+          </Head>
+
+          <Sidebar />
+
+          {children}
+      </>
+    );
+}
+
+export default Layout;
---
+++ pages/index.tsx
@@ -0,0 +1,16 @@
+import Head from 'next/head';
+import Layout from '../components/Layout';
+
+const Index = () => {
+  return (
+    <>
+      <div>
+        <Layout>
+          <h2>Desde Index</h2>
+        </Layout>
+      </div>
+    </>
+  )
+}
+
+export default Index;
